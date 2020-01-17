fcfaf58562f6b9fff903121ccbe04ef44d89e7de
unknown <pratik3098@gmail.com>
2020-01-16 21:41:00 -0500

    functions added

--- ERC20.sol
+++ ERC20.sol
@@ -18,9 +18,36 @@ interface IERC20 {
 
 
 contract Token is IERC20{
-    string  public  name= "George Brown Token";
-    string  public  symbol= "GBC";
+    string  public  name;
+    string  public  symbol;
+    uint    public  decimals;
     uint    public  total_supply;
     address public  owner;
-    construc
+    mapping (address => uint)                   balances;
+    mapping (address => mapping (address => uint)) approvals;
+    
+
+    constructor(uint total_balance) public {
+        name = "George Brown Token";                                  
+        symbol = "GBC";    
+        decimals = 2; 
+        owner=msg.sender;
+        balances[msg.sender] = total_balance;               
+        total_supply = total_balance;                       
+           
+    }
+    
+    function totalSupply() external view returns (uint256){     return total_supply;   }
+    function balanceOf(address acc_owner) external view returns (uint256 balance){  return balances[acc_owner] } ;            }
+    function transfer(address recipient, uint256 amount) external returns (bool success)
+    {
+        
+    }
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool success);
+    function approve(address spender, uint256 amount) external returns (bool success);
+    function allowance(address owner, address spender) external view returns (uint256 remaining);
+    event Transfer(address indexed sender, address indexed recipient, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+    
+
 }
